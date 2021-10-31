8749bee9a8e8426829cdc3bcdbfadf9f406bf63e
Andika Fathin Azfar <deutandev@pm.me>
2021-10-31 23:44:26 +0700

    problem09: Add Bullet prefab & control

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    [SerializeField]
+    private Transform barrelTip;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    private Vector2 lookDirection;
+    private float lookAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition - transform.position);
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
+    }
+
+    private void FireBullet()
+    {
+        GameObject firedBullet = Instantiate(bullet, barrelTip.position, barrelTip.rotation);
+        firedBullet.GetComponent<Rigidbody2D>().velocity = barrelTip.up * 10f;
+    }
+}
