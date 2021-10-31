29708df33e78615fafc58bd5e7d0b8170949d199
Andika Fathin Azfar <deutandev@pm.me>
2021-11-01 02:04:01 +0700

    Problem 10: Tambah scene untuk masuk Problem sebelumnya

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,7 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public void Problem01() {  
+        SceneManager.LoadScene("Problem01");  
+    }
+    public void Problem02() {  
+        SceneManager.LoadScene("Problem02");  
+    }
+    public void Problem03() {  
+        SceneManager.LoadScene("Problem03");  
+    }
+    public void Problem04() {  
+        SceneManager.LoadScene("Problem04");  
+    }
+    public void Problem05() {  
+        SceneManager.LoadScene("Problem05");  
+    }
+    public void Problem06() {  
+        SceneManager.LoadScene("Problem06");  
+    }
+    public void Problem07() {  
+        SceneManager.LoadScene("Problem07");  
+    }
+    public void Problem08() {  
+        SceneManager.LoadScene("Problem08");  
+    }
+    public void Problem09menu() {  
+        SceneManager.LoadScene("Problem09-menu");  
+    }
     public void Problem09() {  
         SceneManager.LoadScene("Problem09");  
+    }
+    public void Problem10() {  
+        SceneManager.LoadScene("Problem09");  
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            SceneManager.LoadScene("Problem10");
+        }
     }
 }
