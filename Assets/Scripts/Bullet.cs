f07ec2e009c86d044b8605e857565e8636bcdc0a
Andika Fathin Azfar <deutandev@pm.me>
2021-11-01 00:11:22 +0700

    problem09: Destroy Bullet saat menabrak Box atau Wall

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject bullet;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Box" || other.gameObject.tag == "Wall")
+        {
+            Destroy(bullet);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
